8a1f0e794a0ce2844ed2f90c1df89704f999678f
AndrewRigg <AndrewJRigg@gmail.com>
2017-10-12 16:43:15 +0100

    Added piece functionality

--- src/myapp/ChessMate.java
+++ src/myapp/ChessMate.java
@@ -6,34 +6,20 @@ package myapp;
 
 import java.util.ArrayList;
 
-import com.sun.speech.freetts.Voice;
-import com.sun.speech.freetts.VoiceManager;
+import com.sun.speech.freetts.*;
 
-import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.application.*;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.CheckMenuItem;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.RadioMenuItem;
-import javafx.scene.control.SeparatorMenuItem;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.geometry.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.scene.image.*;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
 public class ChessMate extends Application {
 
@@ -49,26 +35,27 @@ public class ChessMate extends Application {
 	Image [] whiteImages, blackImages;
 	ArrayList<ImageView> whiteImageViews, blackImageViews, takenWhitePieces, takenBlackPieces;
 	ArrayList<Piece> whitePieces, blackPieces;
+	ArrayList<Integer> rows;
+	ArrayList<Character> cols;
 	ImageView currentPiece;
 	Piece current;
-	int currentCol, currentRow;
 	GridPane board;
 	StackPane clockPane1, clockPane2;
 	boolean piecePicked, whiteTurn;
 	final int IMAGE_TYPES = 6, SQUARE_SIZE = 60, NUMBER_OF_PIECES = 16;
 	int [] indices = {5, 2, 0, 4, 1, 0, 2, 5};
-	ArrayList<Integer> rows;
-	ArrayList<Character> cols;
+	String [] imageLocations = {"Bishop", "King", "Knight", "Pawn", "Queen", "Rook"};	
 	private static final String VOICE = "kevin";
+	int currentCol, currentRow;
 	
 	/**
 	 * Constructor to initialise the chess board, players, clocks, menus
 	 * pieces, images and values.
 	 */
 	public ChessMate () {
-		player1 = new Player("Player1", PlayerType.HUMAN, 0); 
-		player1.playerTurn = true;
+		player1 = new Player("Player1", PlayerType.HUMAN, 0);
 		player2 = new Player("Player2", PlayerType.CPU, 0);
+		player1.playerTurn = true;
 		root = new BorderPane();
 		board = new GridPane();
 		clock1 = new ChessClock(ClockMode.CUSTOM, player1, 5, 10);
@@ -77,26 +64,26 @@ public class ChessMate extends Application {
 		fileMenu = new Menu("File");
 		compMenu = new Menu("Competition");
 		tutorialMenu = new Menu("Tutorial");
+		webMenu = new Menu("Clock");
 		newMenuItem = new MenuItem("New");
 		saveMenuItem = new MenuItem("Save");
 		exitMenuItem = new MenuItem("Exit");
 		p1MenuItem = new CheckMenuItem("Player1");
 		p2MenuItem = new CheckMenuItem("Player2");
-		webMenu = new Menu("Clock");
 		tGroup = new ToggleGroup();
 		mycompItem = new RadioMenuItem("Competition");
 		speedItem = new RadioMenuItem("Speed");
 		customItem = new RadioMenuItem("Custom");
-		takenWhitePieces = new ArrayList<>();
-		takenBlackPieces = new ArrayList<>();
 		whiteImages = new Image [IMAGE_TYPES];
 		blackImages = new Image [IMAGE_TYPES];
+		takenWhitePieces = new ArrayList<>();
+		takenBlackPieces = new ArrayList<>();
 		whiteImageViews = new ArrayList<>();
 		blackImageViews = new ArrayList<>();
 		whitePieces = new ArrayList<>();
 		blackPieces = new ArrayList<>();
-		rows = new ArrayList<Integer>();
-		cols = new ArrayList<Character>();
+		rows = new ArrayList<>();
+		cols = new ArrayList<>();
 		for(int i = 1, a = 'A'; i <= 8; i ++, a++){
 			rows.add(i);
 			cols.add((char) a);
@@ -104,41 +91,27 @@ public class ChessMate extends Application {
 	}
 	
 	public void start(Stage primaryStage) {		
-		
 	    menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
 	    root.setTop(menuBar);
 	    exitMenuItem.setOnAction(actionEvent -> Platform.exit());
 	    fileMenu.getItems().addAll(newMenuItem, saveMenuItem, new SeparatorMenuItem(), exitMenuItem);
+	    tutorialMenu.getItems().addAll(new CheckMenuItem("Rules"), new CheckMenuItem("Matches"), new CheckMenuItem("Moves"));
+	    compMenu.getItems().add(tutorialMenu);
+	    webMenu.getItems().add(p1MenuItem);
+	    webMenu.getItems().add(p2MenuItem);
+	    compMenu.getItems().addAll(mycompItem, speedItem, customItem, new SeparatorMenuItem());
         p1MenuItem.setSelected(true);
-        webMenu.getItems().add(p1MenuItem);
-        p1MenuItem.setSelected(true);
-        webMenu.getItems().add(p2MenuItem);
         mycompItem.setToggleGroup(tGroup);
         mycompItem.setSelected(true);
         speedItem.setToggleGroup(tGroup);
         customItem.setToggleGroup(tGroup);
-        compMenu.getItems().addAll(mycompItem, speedItem, customItem,
-        new SeparatorMenuItem());
-        tutorialMenu.getItems().addAll(
-        new CheckMenuItem("Rules"),
-        new CheckMenuItem("Matches"),
-        new CheckMenuItem("Moves"));
-        compMenu.getItems().add(tutorialMenu);
-        menuBar.getMenus().addAll(fileMenu, webMenu, compMenu);
-	
+        menuBar.getMenus().addAll(fileMenu, compMenu, webMenu);
 		board.setPadding(new Insets(10));
 		board.setHgap(5);
-		board.setVgap(5);
-		String [] imageLocations = {"Bishop", "King", "Knight", "Pawn", "Queen", "Rook"};
-
-		  
-		  
-		  
-		  
+		board.setVgap(5);	  
 		for (int i = 0; i < IMAGE_TYPES; i++) {
 			whiteImages[i] = new Image("res/" + "white" + imageLocations[i] + ".png");
 			blackImages[i] = new Image("res/" + "black" + imageLocations[i] + ".png");
-			//speak(imageLocations[i].substring(0, imageLocations[i].length()-4));
 		}
 		
 		setUpImageViews(whiteImageViews, player1, takenWhitePieces);
@@ -147,30 +120,172 @@ public class ChessMate extends Application {
 		setUpImages(whiteImageViews, whiteImages, indices);
 		setUpImages(blackImageViews, blackImages, indices);
 		
-		setUpPieces(whitePieces, whiteImageViews, imageLocations, indices);
-		setUpPieces(blackPieces, blackImageViews, imageLocations, indices);
+		setUpPieces(whitePieces, whiteImageViews, imageLocations, indices, true);
+		setUpPieces(blackPieces, blackImageViews, imageLocations, indices, false);
 		
 		setUpBoard();
 		
 		addPiecesToBoard(board, whiteImageViews, false);
 		addPiecesToBoard(board, blackImageViews, true);
-	
-		System.out.println("chesses: " + board.getChildren());
-		
+			
 		board.setStyle("-fx-background-color: #336699;");
-		BorderPane p = new BorderPane();
-		Text t = new Text("Hello FX");
-		t.setFont(Font.font("Arial", 60));
-		t.setEffect(new DropShadow());
-		p.setCenter(t);
 		root.setCenter(board);
+		Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 		primaryStage.setScene(new Scene(root));
+		primaryStage.setX(primaryScreenBounds.getMinX());
+		primaryStage.setY(primaryScreenBounds.getMinY());
+		primaryStage.setWidth(primaryScreenBounds.getWidth());
+		primaryStage.setHeight(primaryScreenBounds.getHeight());
 		primaryStage.setTitle("ChessMate");
-		primaryStage.show();
+		primaryStage.show(); 
+	}
+	
+	/**
+	 * Set up all the pieces which will fill the board.  This includes
+	 * setting up all the events for each piece such as moves, removal etc.
+	 * @param pieces
+	 * @param player
+	 * @param takenPieces
+	 */
+	public void setUpImageViews(ArrayList<ImageView> imageViews, Player player, ArrayList<ImageView> takenPieces) {
+		for(int i = 0; i < NUMBER_OF_PIECES; i++) {
+			ImageView imageView = new ImageView();
+			imageView.setFitWidth(SQUARE_SIZE);
+			imageView.setFitHeight(SQUARE_SIZE);
+			final Integer innerI = new Integer(i);
+			imageView.setOnMouseClicked(new EventHandler<MouseEvent>()
+	        {
+	            @Override
+	            public void handle(MouseEvent t) {
+	            	if(player.playerTurn) {
+		            	piecePicked = true;
+		            	currentPiece = imageViews.get(innerI);
+		            	//current = 
+	            	}else {
+	            		if(piecePicked) {
+	            			currentPiece = imageViews.get(innerI);
+	            			takenPieces.add(currentPiece);
+	            			imageViews.remove(currentPiece);
+	            			piecePicked = false;
+	            			swapTurns();
+	            		}
+	            	}
+	            }
+	        });
+			imageViews.add(imageView);
+		}
+	}
+	
+	/**
+	 * Set the associated images of the chess pieces 
+	 * @param pieces
+	 * @param images
+	 */
+	public void setUpImages(ArrayList<ImageView> imageViews, Image [] images, int [] indices) {
+		for(int j = 0; j < 8; j++) {
+			imageViews.get(j+8).setImage(images[3]);
+			imageViews.get(j).setImage(images[indices[j]]);
+		}
+	}
+	
+	/**
+	 * Set the piece objects to contain the coordinates, the image and the name of the piece
+	 * @param pieces
+	 * @param images
+	 * @param imageLocations
+	 * @param indices
+	 */
+	public void setUpPieces(ArrayList<Piece> pieces, ArrayList<ImageView> images, String [] imageLocations, int [] indices, boolean isWhite) {
+		for(int i = 0; i < NUMBER_OF_PIECES; i++) {
+			Piece piece;
+			String colour = (isWhite) ? "White " : "Black ";
+			if(i > 7){
+				piece = new Piece(colour + imageLocations[3] + " " + (i - 7), imageLocations[3], 0, 0, images.get(i));
+			}else{
+				piece = new Piece(colour + imageLocations[indices[i]], imageLocations[indices[i]], 0, 0, images.get(i));
+			}
+			System.out.println("Piece: " + piece.getName());
+			pieces.add(piece);
+		}
+	}
+	
+	/**
+	 * Set up the board to have an 8x8 grid with a border 1 square thick,
+	 * stack panes and clock timers
+	 */
+	public void setUpBoard(){
+		char start = 'a';
+		piecePicked = false;
+		for(int a = 0; a < 10; a++) {
+			for(int b = 0; b < 10; b++) {
+				StackPane pane = new StackPane();
+				pane.setOnMouseClicked(new EventHandler<MouseEvent>()
+		        {
+		            @Override
+		            public void handle(MouseEvent t) {
+		            	if(piecePicked) {
+		            		int col = GridPane.getColumnIndex(pane);
+		            		int row = GridPane.getRowIndex(pane);
+		            		movePiece(board, current, currentPiece, col, row);
+		            		piecePicked = false;
+		            		String command = getStringCommand(currentPiece, currentCol, currentRow, col, row);
+		            		speak(command);
+		            		swapTurns();
+		            	}
+		            }
+		        });
+				Rectangle rec = new Rectangle(SQUARE_SIZE, SQUARE_SIZE);
 		
-		//GamePlay game = new GamePlay();
-		System.out.println("chesses: " + board.getChildren().get(0));
-		//updateBoard((StackPane)board.getChildren().get(0), player1, clock1);
+				if(a == 0 && b == 0) {
+					pane.setAlignment(Pos.TOP_LEFT);
+					updateBoard(pane, player1, clock1);
+					pane.getChildren().add(updateClockOnBoard(player1, clock1));
+					//pane = clockPane1;
+				}
+				
+				if(a == 9 && b == 0) {
+					pane.setAlignment(Pos.TOP_RIGHT);
+					updateBoard(pane, player2, clock2);
+					pane.getChildren().add(updateClockOnBoard(player2, clock2));
+					//pane = clockPane2;
+				}
+				
+				createCheckerBoard(rec, a, b);
+				
+				
+				if( b > 0 && b < 9 && a == 0) {
+					Text text = new Text(""+ (9-b));
+					pane.getChildren().add(text);
+					pane.setAlignment(Pos.CENTER_RIGHT);
+				}
+				if( a > 0 && a < 9 && b == 9) {
+					
+					Text text = new Text(""+ start++);
+					pane.getChildren().add(text);
+					pane.setAlignment(Pos.TOP_CENTER);
+				}
+				
+				pane.getChildren().addAll(rec);
+				board.add(pane, a, b);
+			}
+		}
+	}
+	
+	/**
+	 * Add the ImageViews of chess pieces to the board
+	 * @param board
+	 * @param pieces
+	 */
+	public void addPiecesToBoard(GridPane board, ArrayList<ImageView> pieces, boolean black) {
+		for(int i = 0; i < 8; i++) {
+			if(black) {
+				board.add(pieces.get(i), i+1, 1);
+				board.add(pieces.get(i+8), i+1, 2);
+			}else {
+				board.add(pieces.get(i), i+1, 8);
+				board.add(pieces.get(i+8), i+1, 7);
+			}
+		}
 	}
 	
 	/**
@@ -210,134 +325,6 @@ public class ChessMate extends Application {
 	}
 	
 	/**
-	 * Set up all the pieces which will fill the board.  This includes
-	 * setting up all the events for each piece such as moves, removal etc.
-	 * @param pieces
-	 * @param player
-	 * @param takenPieces
-	 */
-	public void setUpImageViews(ArrayList<ImageView> imageViews, Player player, ArrayList<ImageView> takenPieces) {
-		for(int i = 0; i < NUMBER_OF_PIECES; i++) {
-			ImageView imageView = new ImageView();
-			imageView.setFitWidth(SQUARE_SIZE);
-			imageView.setFitHeight(SQUARE_SIZE);
-			final Integer innerI = new Integer(i);
-			imageView.setOnMouseClicked(new EventHandler<MouseEvent>()
-	        {
-	            @Override
-	            public void handle(MouseEvent t) {
-	            	if(player.playerTurn) {
-		            	piecePicked = true;
-		            	currentPiece = imageViews.get(innerI);
-		            	currentCol = 2;
-		            	currentRow = 2;
-	            	}else {
-	            		if(piecePicked) {
-	            			takenPieces.add(imageViews.get(innerI));
-	            			//pieces.remove(piece);
-	            			piecePicked = false;
-	            			swapTurns();
-	            			//pane.remove(pieces[innerI]);
-	            		}
-	            	}
-	            }
-	        });
-			imageViews.add(imageView);
-		}
-	}
-	
-	/**
-	 * Set the associated images of the chess pieces 
-	 * @param pieces
-	 * @param images
-	 */
-	public void setUpImages(ArrayList<ImageView> imageViews, Image [] images, int [] indices) {
-		for(int j = 0; j < 8; j++) {
-			imageViews.get(j+8).setImage(images[3]);
-			imageViews.get(j).setImage(images[indices[j]]);
-		}
-	}
-	
-	/**
-	 * Set the piece objects to contain the coordinates, the image and the name of the piece
-	 * @param pieces
-	 * @param images
-	 * @param imageLocations
-	 * @param indices
-	 */
-	public void setUpPieces(ArrayList<Piece> pieces, ArrayList<ImageView> images, String [] imageLocations, int [] indices) {
-		for(int i = 0; i < NUMBER_OF_PIECES; i++) {
-			Piece piece;
-			if(i > 7){
-				piece = new Piece(imageLocations[3], 0, 0, images.get(i));
-			}else{
-				piece = new Piece(imageLocations[indices[i]], 0, 0, images.get(i));
-			}
-			pieces.add(piece);
-		}
-	}
-	
-	public void setUpBoard(){
-		char start = 'a';
-		piecePicked = false;
-		for(int a = 0; a < 10; a++) {
-			for(int b = 0; b < 10; b++) {
-				StackPane pane = new StackPane();
-				pane.setOnMouseClicked(new EventHandler<MouseEvent>()
-		        {
-		            @Override
-		            public void handle(MouseEvent t) {
-		            	if(piecePicked) {
-		            		int col = GridPane.getColumnIndex(pane);
-		            		int row = GridPane.getRowIndex(pane);
-		            		movePiece(board, currentPiece, col, row);
-		            		piecePicked = false;
-		            		String command = getStringCommand(currentPiece, currentCol, currentRow, col, row);
-		            		speak(command);
-		            		swapTurns();
-		            	}
-		            }
-		        });
-				Rectangle rec = new Rectangle();
-				rec.setWidth(SQUARE_SIZE);
-				rec.setHeight(SQUARE_SIZE);		
-		
-				if(a == 0 && b == 0) {
-					pane.setAlignment(Pos.TOP_LEFT);
-					updateBoard(pane, player1, clock1);
-					pane.getChildren().add(updateClockOnBoard(player1, clock1));
-					//pane = clockPane1;
-				}
-				
-				if(a == 9 && b == 0) {
-					pane.setAlignment(Pos.TOP_RIGHT);
-					updateBoard(pane, player2, clock2);
-					pane.getChildren().add(updateClockOnBoard(player2, clock2));
-					//pane = clockPane2;
-				}
-				
-				createCheckerBoard(rec, a, b);
-				
-				
-				if( b > 0 && b < 9 && a == 0) {
-					Text text = new Text(""+ (9-b));
-					pane.getChildren().add(text);
-					pane.setAlignment(Pos.CENTER_RIGHT);
-				}
-				if( a > 0 && a < 9 && b == 9) {
-					
-					Text text = new Text(""+ start++);
-					pane.getChildren().add(text);
-					pane.setAlignment(Pos.TOP_CENTER);
-				}
-				
-				pane.getChildren().addAll(rec);
-				board.add(pane, a, b);
-			}
-		}
-	}
-	
-	/**
 	 * Swap the boolean value determining whether it is that player's
 	 * turn and switch the clock which is counting down.
 	 */
@@ -358,25 +345,6 @@ public class ChessMate extends Application {
 		updateClockOnBoard(player, clock);
 		System.out.println("Entered updateBoard");
 	}
-
-	
-	
-	/**
-	 * Add the ImageViews of chess pieces to the board
-	 * @param board
-	 * @param pieces
-	 */
-	public void addPiecesToBoard(GridPane board, ArrayList<ImageView> pieces, boolean black) {
-		for(int i = 0; i < 8; i++) {
-			if(black) {
-				board.add(pieces.get(i), i+1, 1);
-				board.add(pieces.get(i+8), i+1, 2);
-			}else {
-				board.add(pieces.get(i), i+1, 8);
-				board.add(pieces.get(i+8), i+1, 7);
-			}
-		}
-	}
 	
 	/**
 	 * Move a piece from one square to another by removing the
@@ -386,9 +354,11 @@ public class ChessMate extends Application {
 	 * @param toCol
 	 * @param toRow
 	 */
-	public void movePiece(GridPane board, ImageView piece, int toCol, int toRow) {
-		board.getChildren().remove(piece);
-		board.add(piece, toCol, toRow);
+	public void movePiece(GridPane board, Piece piece, ImageView image, int toCol, int toRow) {
+		board.getChildren().remove(image);
+		board.add(image, toCol, toRow);
+		piece.col = toCol;
+		piece.row = toRow;
 	}
 	
 	/**
--- src/myapp/Piece.java
+++ src/myapp/Piece.java
@@ -4,12 +4,14 @@ import javafx.scene.image.ImageView;
 public class Piece {
 
 	String name = "";
+	String type = "";
 	int col;
 	int row;
 	ImageView image;
 	
-	public Piece(String name, int col, int row, ImageView image) {
+	public Piece(String name, String type, int col, int row, ImageView image) {
 		this.name = name;
+		this.type = type;
 		this.col = col;
 		this.row = row;
 		this.image = image;
@@ -28,7 +30,12 @@ public class Piece {
 		this.name = name;
 	}
 	
-	//As above
+	//This might be needed if e.g. a pawn is promoted to a queen
+	public void setType(String type){
+		this.type = type;
+	}
+	
+	//This might be needed if e.g. a pawn is promoted to a queen
 	public void setImage(ImageView image){
 		this.image = image;
 	}
